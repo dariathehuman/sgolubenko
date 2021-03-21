f0483b9de2c8aa6cf7aad6ba0eaaf0321481fbca
dariathehuman <nabatova.darya@protonmail.com>
2021-03-21 17:53:42 +0300

    changed js

--- source/js/main.js
+++ source/js/main.js
@@ -1,18 +1,24 @@
-const menuBtn = document.querySelector('.toggle');
-const menu = document.querySelector('.nav');
-const container = document.querySelector('.container');
+const menuToggle = function () {
+    const menuBtn = document.querySelector('.toggle');
+    const menu = document.querySelector('.nav');
+    const container = document.querySelector('.container');
 
-menuBtn.addEventListener('click', function (evt) {
-    evt.preventDefault()
-    menuBtn.classList.toggle('toggle--closed');
-    menuBtn.classList.toggle('toggle--opened');
-    menu.classList.toggle('nav--closed');
-    menu.classList.toggle('nav--opened');
-    container.classList.toggle('container--nav');
-});
+    menuBtn.addEventListener('click', function (evt) {
+        evt.preventDefault();
+        menuBtn.classList.toggle('toggle--closed');
+        menuBtn.classList.toggle('toggle--opened');
+        menu.classList.toggle('nav--closed');
+        menu.classList.toggle('nav--opened');
+        container.classList.toggle('container--nav');
+    });
+};
 
-const year = document.querySelector('.year');
-year.innerHTML = new Date().getFullYear();
+const changeYear = function() {
+    const year = document.querySelector('.year');
+    year.innerHTML = new Date().getFullYear();
+};
 
+menuToggle();
+changeYear();
 
 new WOW().init();
